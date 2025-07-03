b6a785a4359fd03458d335427e6e406ffb6ec708
Josue Antonio Arellano Berlanga <144313678+josefprox@users.noreply.github.com>
2025-07-03 04:53:17 -0600

    colores cambiados de cards

--- theme/indexadmin.php
+++ theme/indexadmin.php
@@ -160,6 +160,83 @@ if (isset($_POST['curso']) && isset($_POST['duracion'])) {
   transform: scale(1.05);
   box-shadow: 0 12px 24px rgba(0, 0, 0, 0.2);
 }
+.card {
+  background-color:rgb(238, 112, 112);
+  background-image: linear-gradient(43deg,rgb(235, 74, 74) 0%,rgb(252, 252, 252) 46%,rgb(156, 16, 16) 100%);
+  border-radius: 8px;
+  color: white;
+  overflow: hidden;
+  position: relative;
+  transform-style: preserve-3d;
+  perspective: 1000px;
+  transition: all 0.5s cubic-bezier(0.23, 1, 0.320, 1);
+  cursor: pointer;
+}
+
+.card-content {
+  padding: 20px;
+  position: relative;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: white;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100%;
+}
+
+.card-content .card-title {
+  font-size: 24px;
+  font-weight: 700;
+  color: inherit;
+  text-transform: uppercase;
+}
+
+.card-content .card-para {
+  color: inherit;
+  opacity: 0.8;
+  font-size: 14px;
+}
+
+.card:hover {
+  transform: rotateY(10deg) rotateX(10deg) scale(1.05);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+}
+
+.card:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(transparent, rgba(0, 0, 0, 0.1));
+  transition: transform 0.5s cubic-bezier(0.23, 1, 0.320, 1);
+  z-index: 1;
+}
+
+.card:hover:before {
+  transform: translateX(-100%);
+}
+
+.card:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(transparent, rgba(0, 0, 0, 0.1));
+  transition: transform 0.5s cubic-bezier(0.23, 1, 0.320, 1);
+  z-index: 1;
+}
+
+.card:hover:after {
+  transform: translateX(100%);
+}
+
     </style>
 </head>
 
@@ -300,10 +377,10 @@ if (isset($_SESSION['usuario'])) {
         // Título
         if ($usuario) {
             echo '<h5 class="card-title" style="font-size: 22px; font-weight: bold; color: #d32f2f;">' . htmlspecialchars($usuario) . '</h5>';
-            echo '<p style="font-weight: 500; margin-top: 10px;">Cursos asignados:</p>';
+            echo '<p style="font-weight: 500; margin-top: 10px; color:rgb(0, 0, 0);" >Cursos asignados:</p>';
         } else {
             echo '<h5 class="card-title" style="font-size: 22px; font-weight: bold; color: #388e3c;">Cursos disponibles</h5>';
-            echo '<p style="font-weight: 500; margin-top: 10px;">Para asignar a los usuarios:</p>';
+            echo '<p style="font-weight: 500; margin-top: 10px; color:rgb(27, 43, 28);">Para asignar a los usuarios:</p>';
         }
 
         // Lista de cursos
